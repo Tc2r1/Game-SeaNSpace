1d2428f41767dd6961753f179f28a04597d89a7b
Cinders-P <askcinders@outlook.com>
2017-08-31 21:05:50 -0700

    libGDX boilerplate

---
+++ desktop/src/com/dragonjam/aliens/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dragonjam.aliens.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dragonjam.aliens.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
