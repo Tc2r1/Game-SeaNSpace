87639d3fd8c6a4e01d473eadec8d6060005b239b
Rane Lafraze <lafrazerl@gmail.com>
2017-09-01 16:28:17 -0400

    Define the window width and height

--- desktop/src/com/dragonjam/aliens/desktop/DesktopLauncher.java
+++ desktop/src/com/dragonjam/aliens/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package com.dragonjam.aliens.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.dragonjam.aliens.GameMain;
+import com.dragonjam.game.GameMain;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 720;
 		new LwjglApplication(new GameMain(), config);
 	}
 }
